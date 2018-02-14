76830175380d3b6f54150094607cb28c43834842
Rich <rbaird@richbairdportfolio.com>
2018-02-14 16:27:02 -0700

    v1.3.0

---
+++ dist/browser/services/System.d.ts
@@ -0,0 +1,22 @@
+import * as paramTypes from '../../types/SystemTypes';
+import { Connection, XmlmcResponse } from '../Connection';
+import { XmlMethodCall } from '..';
+export declare class System {
+    private connection;
+    private xmlmc;
+    private service;
+    private paramMap;
+    constructor(connection: Connection, xmlmc: XmlMethodCall);
+    getFileTypeRestriction(optionalParams?: paramTypes.getFileTypeRestrictionParams): Promise<XmlmcResponse>;
+    getSystemInfo(optionalParams?: paramTypes.getSystemInfoParams): Promise<XmlmcResponse>;
+    getTimezoneDetails(optionalParams?: paramTypes.getTimezoneDetailsParams): Promise<XmlmcResponse>;
+    getTimezoneList(optionalParams?: paramTypes.getTimezoneListParams): Promise<XmlmcResponse>;
+    getTimezoneOffset(optionalParams?: paramTypes.getTimezoneOffsetParams): Promise<XmlmcResponse>;
+    multiClipAddItem(personalClipboard: boolean, name: string, itemData: string, optionalParams?: paramTypes.multiClipAddItemParams): Promise<XmlmcResponse>;
+    multiClipDeleteItem(itemId: Object, optionalParams?: paramTypes.multiClipDeleteItemParams): Promise<XmlmcResponse>;
+    multiClipGetItemData(itemId: number, optionalParams?: paramTypes.multiClipGetItemDataParams): Promise<XmlmcResponse>;
+    multiClipGetTree(optionalParams?: paramTypes.multiClipGetTreeParams): Promise<XmlmcResponse>;
+    multiClipSetItemData(itemId: Object, itemData: string, optionalParams?: paramTypes.multiClipSetItemDataParams): Promise<XmlmcResponse>;
+    offsetEpochTime(epochTime: number, optionalParams?: paramTypes.offsetEpochTimeParams): Promise<XmlmcResponse>;
+    sendProductFeedback(message: string, optionalParams?: paramTypes.sendProductFeedbackParams): Promise<XmlmcResponse>;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ var path = require('path');
 var webpack = require('webpack');
 var plugins = [];
 
-//plugins.push(new DtsBundlePlugin());
 plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
 
 module.exports = [{
-    target: 'web',
     entry: './src/browser/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist', 'browser'),
-        filename: 'index.min.js'
+        filename: 'index.min.js',
+        library: 'xmlmc',
+        libraryTarget: 'umd',
     },
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
@@ -55,11 +55,12 @@ module.exports = [{
     plugins: plugins,
 },
     {
-        target: 'web',
         entry: './src/browser/index.ts',
         output: {
             path: path.resolve(__dirname, 'dist', 'browser'),
-            filename: 'index.js'
+            filename: 'index.js',
+            library: 'xmlmc',
+            libraryTarget: 'umd',
         },
         resolve: {
             // Add `.ts` and `.tsx` as a resolvable extension.
@@ -85,6 +86,52 @@ module.exports = [{
                                             "FireFox <= 52",
                                             "Explorer 10"
                                         ]
+                                    }
+                                }
+                            ]
+                        ],
+                        "plugins": [
+                            "syntax-dynamic-import",
+                            "dynamic-import-node",
+                            "transform-object-rest-spread",
+                            "transform-runtime"
+                        ]
+                    }
+                }
+                }
+            ]
+        },
+
+    },
+    {
+        entry: './src/index.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index.js',
+            library: 'xmlmc',
+            libraryTarget: 'umd',
+        },
+        resolve: {
+            // Add `.ts` and `.tsx` as a resolvable extension.
+            extensions: ['.ts', '.tsx', '.js']
+        },
+
+        externals: {
+            'tough-cookie': 'CookieJar'
+        },
+        module: {
+            rules: [
+                // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+                {
+                    test: /\.tsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/, options: {
+                    useBabel: true,
+                    babelOptions: {
+                        "presets": [
+                            [
+                                "env",
+                                {
+                                    "targets": {
+                                        "node": "6.10"
                                     }
                                 }
                             ]
